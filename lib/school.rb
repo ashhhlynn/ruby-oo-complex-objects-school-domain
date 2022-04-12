fc2e2e286b57ca8a4b9018d9c2caa9962424e4fb
ashhhlynn <ashleylynnwelch@gmail.com>
2022-04-12 16:44:25 -0500

    commit tree 3292df3b346a67366f7d8241a61a0ea7adfa3757

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    attr_accessor :roster
+
+
+    def add_student(student_name, grade)
+        if @roster[grade]
+            @roster[grade] << student_name
+        else 
+            @roster[grade]=[] 
+            @roster[grade] << student_name
+        end
+    end 
+
+  def grade(grade)
+    @roster[grade]
+  end   
+
+  def sort
+    sorted_roster={}
+    @roster.each do |grade, students|
+        sorted_students=students.sort
+        sorted_roster[grade] = sorted_students
+    end
+    sorted_roster
+end
+
+end 
+    school = School.new("Bayside High School")
